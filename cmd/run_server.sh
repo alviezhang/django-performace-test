c62059386d002fece2f36cab117b3b7aa4a773f2
Alvie Zhang <alvie.zhang@gmail.com>
2021-01-06 19:19:12 +0800

    feat: change port to 8000

--- cmd/run_server.sh
+++ cmd/run_server.sh
@@ -2,4 +2,4 @@
 
 export PYTHONPATH=django_asgi
 
-exec poetry run gunicorn asgi:application -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8080
+exec poetry run gunicorn asgi:application -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000
